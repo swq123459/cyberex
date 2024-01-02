c6fdaa1f7f0d1ae08adc292ce802f4d269ad14a1
hdl <1305857710@qq.com>
2024-01-02 16:15:27 +0800

    ✨ feature: xffi c_char to string

--- src/xffi/sto.rs
+++ src/xffi/sto.rs
@@ -1,4 +1,4 @@
-use std::os::raw::c_char;
+use std::{ffi::CStr, os::raw::c_char};
 #[allow(clippy::needless_range_loop)]
 pub fn string_to_array<const COUNT: usize>(s: &str) -> [c_char; COUNT] {
     let mut a = [0 as c_char; COUNT];
@@ -7,4 +7,12 @@ pub fn string_to_array<const COUNT: usize>(s: &str) -> [c_char; COUNT] {
         a[i] = s.as_bytes()[i] as c_char;
     }
     a
+}
+
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+pub fn cchar_to_string(c_char: *const c_char) -> String {
+    if c_char as usize == 0 {
+        return String::new();
+    }
+    unsafe { CStr::from_ptr(c_char).to_string_lossy().into_owned() }
 }
--- tests/test_sto.rs
+++ tests/test_sto.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use cyberex::xffi::sto::*;
+    use std::os::raw::c_char;
 
     #[test]
     fn test_string_to_array() {
@@ -17,6 +18,18 @@ mod tests {
             assert_eq!(a, a_must);
 
             assert!(a.len() == 2);
+        }
+    }
+
+    #[test]
+    fn test_cchar_to_string() {
+        {
+            let c: *const c_char = std::ptr::null();
+            assert_eq!(cchar_to_string(c), String::new());
+        }
+        {
+            let c: *const c_char = "hello\0".as_ptr() as _;
+            assert_eq!(cchar_to_string(c), "hello".to_string());
         }
     }
 }
