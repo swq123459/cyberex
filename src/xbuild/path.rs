dbc2842f67fda9815eca1b7d589b631e0e51362c
Su <469446490@qq.com>
2024-04-05 14:24:42 +0800

    ✨ feature:  parse path from env

--- src/xbuild/path.rs
+++ src/xbuild/path.rs
@@ -1,13 +1,20 @@
 use crate::plat::dist::{plat_dist, PlatDist};
 use crate::xpath::path::path_to_string;
 use std::env;
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
+
+#[derive(Default, Debug, PartialEq, Eq)]
+pub struct DevPath {
+    pub include: PathBuf,
+    pub lib: PathBuf,
+}
 
 fn get_dist_lib_name() -> String {
     match plat_dist() {
         PlatDist::Rh => "lib64",
         PlatDist::Debian => "lib",
-        _ => "lib",
+        PlatDist::Other => "lib",
     }
     .to_string()
 }
@@ -22,6 +29,34 @@ where
     let lib_alt = if lib_must.ends_with("64") { "lib" } else { "lib64" };
     let out = if lib_must.exists() { lib_must } else { p.join(lib_alt) };
     path_to_string(out)
+}
+pub fn include_path_of_root<Paths>(lib_root: Paths) -> String
+where
+    Paths: AsRef<std::path::Path>,
+    Paths: std::fmt::Display,
+{
+    let out = lib_root.as_ref().join("include");
+    path_to_string(out)
+}
+
+pub fn dev_path_of_root<Paths>(lib_root: Paths) -> DevPath
+where
+    Paths: AsRef<std::path::Path>,
+    Paths: std::fmt::Display,
+{
+    DevPath {
+        include: PathBuf::from(include_path_of_root(&lib_root)),
+        lib: PathBuf::from(lib_path_of_root(lib_root)),
+    }
+}
+pub fn dev_path_of_root_env<K>(lib_root_key: K) -> DevPath
+where
+    K: AsRef<OsStr>,
+{
+    match env::var(lib_root_key) {
+        Err(_) => DevPath::default(),
+        Ok(lib_root) => dev_path_of_root(lib_root),
+    }
 }
 
 pub fn cargo_profile_dir() -> String {
@@ -73,10 +108,25 @@ mod tests {
                 r#"D:\code\thirdlib\EaxLibrary\EaxComponent\gb_media\build\gb-media\thirdlib\ffmpeg\lib"#
             );
         } else if cfg!(linux) {
-            assert_eq!(
-                lib_path_of_root("/workspace/cyberex/thirdlib/catch2"),
-                "/workspace/cyberex/thirdlib/catch2/lib"
-            );
+            let catch_root = "/workspace/cyberex/thirdlib/catch2";
+            let catch_lib = "/workspace/cyberex/thirdlib/catch2/lib";
+            let catch_include = "/workspace/cyberex/thirdlib/catch2/include";
+
+            assert_eq!(lib_path_of_root(catch_root), catch_lib);
+            assert_eq!(include_path_of_root(catch_root), catch_include);
+            let dev_path = dev_path_of_root(catch_root);
+            assert_eq!(dev_path.lib.display().to_string(), catch_lib);
+            assert_eq!(dev_path.include.display().to_string(), catch_include);
+
+            std::env::set_var("FUCKYOU_ROOT", "debug");
+            let dev_path = dev_path_of_root_env(catch_root);
+            assert_eq!(dev_path.lib.display().to_string(), "debug/lib");
+            assert_eq!(dev_path.include.display().to_string(), "debug/include");
+
+
+            let dev_path = dev_path_of_root_env("noexist");
+            assert_eq!(dev_path, DevPath::default());
+
         }
     }
 
