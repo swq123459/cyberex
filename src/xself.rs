86c9a4bce58a00a831e383ce543019a344f112bb
Su <469446490@qq.com>
2024-04-10 00:17:32 +0800

    😕 wip:

--- src/xself.rs
+++ src/xself.rs
@@ -1,5 +1,12 @@
 use std::pin::Pin;
 
+/**
+
+   # Safety
+
+   This function is unsafe. You must guarantee that you will never move the data out of the
+   mutable reference you receive when you call this function, so that the invariants on the Pin type can be upheld.
+*/
 pub unsafe fn self_mut_from_pinbox<T>(p: &mut Pin<Box<T>>) -> &mut T {
     p.as_mut().get_unchecked_mut()
 }
