4ca23e425ab97b116748f5e524d2c56935b1cc07
Su <469446490@qq.com>
2024-04-10 00:12:15 +0800

    😕 wip:

--- src/xself.rs
+++ src/xself.rs
@@ -1,8 +1,9 @@
 use std::pin::Pin;
 
 pub unsafe fn self_mut_from_pinbox<T>(p: &mut Pin<Box<T>>) -> &mut T {
-    Pin::get_unchecked_mut(Pin::as_mut(p))
+    p.as_mut().get_unchecked_mut()
 }
 pub fn self_from_pinbox<T>(p: &Pin<Box<T>>) -> &T {
-    Pin::get_ref(Pin::as_ref(p))
+    p.as_ref().get_ref()
+    // Pin::get_ref(Pin::as_ref(p))
 }
