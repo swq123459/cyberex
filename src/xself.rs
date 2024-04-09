647c0c6283a89b12f68bb57e2902c9bf70b5c60f
Su <469446490@qq.com>
2024-04-10 00:13:50 +0800

    😕 wip:

--- src/xself.rs
+++ src/xself.rs
@@ -5,5 +5,4 @@ pub unsafe fn self_mut_from_pinbox<T>(p: &mut Pin<Box<T>>) -> &mut T {
 }
 pub fn self_from_pinbox<T>(p: &Pin<Box<T>>) -> &T {
     p.as_ref().get_ref()
-    // Pin::get_ref(Pin::as_ref(p))
 }
